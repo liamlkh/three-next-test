cf6f59f31ef4befba4d389c3e146ed7df6f2f3c9
Liam <>
2023-02-16 15:16:33 -0500

    no message

--- src/components/canvas/Scene.tsx
+++ src/components/canvas/Scene.tsx
@@ -2,7 +2,6 @@ import { Canvas } from '@react-three/fiber'
 import { Environment, OrbitControls, PerspectiveCamera, Preload } from '@react-three/drei'
 
 export default function Scene({ children, ...props }) {
-  // Everything defined in here will persist between route changes, only children are swapped
   return (
     <Canvas {...props}>
       <PerspectiveCamera makeDefault position={[0, 0, 50]} fov={60} zoom={0.9} />
